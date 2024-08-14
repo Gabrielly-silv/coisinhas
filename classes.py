f47137ffbbc660af78217fd01b364a25221beb03
Gabrielly-silv <163124582+Gabrielly-silv@users.noreply.github.com>
2024-08-14 11:58:18 -0300

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,15 @@
+class calculadora:
+
+    def soma(num1,num2):
+        return num1 + num2 
+    
+    def sub(num1,num2):
+        return num1 - num2
+    
+    def div(num1,num2):
+        return num1 / num2 
+    
+    def mult(num1,num2):
+        return num1 * num2
+
+print ()
